5dde983a304d5fac1b5dfc4e0fbf1245a9857362
Ahmed-T-Fathy <ahmed.tarek1244@gmail.com>
2024-10-30 15:34:28 +0300

    first commit

---
+++ send.js
@@ -0,0 +1,20 @@
+const amqplib = require('amqplib');
+
+const queueName = "hello";
+
+const msg = "hello world";
+
+const sendMsg = async () => {
+    const connection = await amqplib.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+    await channel.assertQueue(queueName, { durable: false });
+    channel.sendToQueue(queueName, Buffer.from(msg));
+    console.log('Sent: ', msg);
+    setTimeout(()=>{
+        connection.close();
+        process.exit(0);
+
+    },500)
+
+}
+await sendMsg();
